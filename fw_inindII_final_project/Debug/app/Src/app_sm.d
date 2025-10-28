67e9e71445336d2dfeeeba901ffc61f88bfd6c35
Luiz Pedro Bittencourt <luizpedrob65@gmail.com>
2025-10-28 16:06:14 -0300

    Refreshing files

--- fw_inindII_final_project/Debug/app/Src/app_sm.d
+++
@@ -1,3 +0,0 @@
-app/Src/app_sm.o: ../app/Src/app_sm.c \
- /home/pedro-bittencourt/ININD2_F411_FINAL/fw_inindII_final_project/app/Inc/app_sm.h
-/home/pedro-bittencourt/ININD2_F411_FINAL/fw_inindII_final_project/app/Inc/app_sm.h:
--- fw_inindII_final_project/Debug/app/Src/control.d
+++
@@ -1,3 +0,0 @@
-app/Src/control.o: ../app/Src/control.c \
- /home/pedro-bittencourt/ININD2_F411_FINAL/fw_inindII_final_project/app/Inc/control.h
-/home/pedro-bittencourt/ININD2_F411_FINAL/fw_inindII_final_project/app/Inc/control.h:
